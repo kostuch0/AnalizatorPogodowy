b622d6e1a127131458a491e476c965f84a6892ab
Grzegorz <@>
2020-01-15 15:39:07 +0100

    Delikatne zmiany w home

--- react/Test/client/src/components/home/home.js
+++ react/Test/client/src/components/home/home.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
+
+
 import './home.css'
 //import './home.css';
 const Home = ()=> {
 
   return (
-    
         <div class='content'>
+            <center><Plot
+            data={[
+              {
+                x: [2001, 2002, 2003],
+                y: [21, 3, 7],
+                type: 'scatter',
+                mode: 'lines+markers',
+                marker: {color: 'red'},
+              },
+              
+            ]}
+            layout={ {width: 800, height: 600, title: 'Piękny wykres danych bez sensu'} }
+            />
+            </center>
            Dzida składa się z przeddzidzia dzidy, śróddzidzia dzidy, jak i zadzidzia dzidy. Przeddzidzie dzidy składa się z przeddzidzia przeddzidzia dzidy, śróddzidzia przeddzidzia dzidy i, co wymaga podkreślenia, zadzidzia przeddzidzia dzidy. Ogólnie znanym faktem jest, że śróddzidzie dzidy składa się z przeddzidzia śróddzidzia dzidy, śróddzidzia śróddzidzia dzidy, jak i zadzidzia śróddzidzia dzidy. Zadzidzie dzidy składa się z przeddzidzia zadzidzia dzidy, śróddzidzia zadzidzia dzidy i zadzidzia zadzidzia dzidy. Nie można nie wspomnieć o tym, że przeddzidzie przeddzidzia dzidy składa się z przeddzidzia przeddzidzia przeddzidzia dzidy, śróddzidzia przeddzidzia przeddzidzia dzidy i zadzidzia przeddzidzia przeddzidzia dzidy. A więc skoro śróddzidzie przeddzidzia dzidy składa się z przeddzidzia śróddzidzia przeddzidzia dzidy, śróddzidzia śróddzidzia przeddzidzia dzidy, jak również zadzidzia śróddzidzia przeddzidzia dzidy, to logicznym jest, że zadzidzie przeddzidzia dzidy składa się z przeddzidzia zadzidzia przeddzidzia dzidy, śróddzidzia zadzidzia przeddzidzia dzidy, jak i zadzidzia zadzidzia przeddzidzia dzidy. Przeddzidzie śróddzidzia dzidy składa się z przeddzidzia przeddzidzia śróddzidzia dzidy, śróddzidzia przeddzidzia śróddzidzia dzidy, jak również zadzidzia przeddzidzia śróddzidzia dzidy. Śróddzidzie śróddzidzia dzidy składa się z przeddzidzia śróddzidzia śróddzidzia dzidy, śróddzidzia śróddzidzia śróddzidzia dzidy, a także z zadzidzia śróddzidzia śróddzidzia dzidy. Kontynuując: zadzidzie śróddzidzia dzidy składa się z przeddzidzia zadzidzia śróddzidzia dzidy, śróddzidzia zadzidzia śróddzidzia dzidy i zadzidzia zadzidzia śróddzidzia dzidy. Przeddzidzie zadzidzia dzidy składa się z przeddzidzia przeddzidzia zadzidzia dzidy, śróddzidzia przeddzidzia zadzidzia dzidy, jak również zadzidzia przeddzidzia zadzidzia dzidy. Śróddzidzie zadzidzia dzidy składa się z przeddzidzia śróddzidzia zadzidzia dzidy, śróddzidzia śróddzidzia zadzidzia dzidy i zadzidzia śróddzidzia zadzidzia dzidy. Przede wszystkim zadzidzie zadzidzia dzidy składa się z przeddzidzia zadzidzia zadzidzia dzidy, śróddzidzia zadzidzia zadzidzia dzidy i zadzidzia zadzidzia zadzidzia dzidy.
         </div>
+
     
   );
 }
